0ddd2200b76cce98bdaa5133de7b8466deec95e4
BilLogic <BilLogic@users.noreply.github.com>
2026-03-05 22:06:55 +0000

    deploy: 526f3d3522d69471f66d55a1f6a70487a327bbd0

---
+++ storybook/assets/Forms.stories-B7dWMeWz.js
@@ -0,0 +1,13 @@
+import{j as r}from"./jsx-runtime-BO8uF4Og.js";import{F as a}from"./Panels-C3SddXDF.js";import{l as e,C as m,B as l,f as p}from"./Grid-DeyImMHg.js";import"./index-D4H_InIO.js";import"./useMediaQuery-BLMSMJgh.js";import"./index-CZVi18Wq.js";import"./index-Dd8bRu6S.js";const j={component:a,title:"SDS Compositions/Forms",parameters:{layout:"centered"}},o={name:"Forms",render:i=>r.jsxs(a,{...i,onSubmit:()=>{},children:[r.jsx(e,{label:"Email"}),r.jsx(e,{label:"Password"}),r.jsx(m,{label:"Label",description:"Description"}),r.jsx(l,{align:"justify",children:r.jsx(p,{onPress:()=>{},variant:"primary",children:"Register"})})]})};var t,s,n;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  name: "Forms",
+  render: args => <FormBox {...args} onSubmit={() => {}}>
+      <InputField label="Email" />
+      <InputField label="Password" />
+      <CheckboxField label="Label" description="Description" />
+      <ButtonGroup align="justify">
+        <Button onPress={() => {}} variant="primary">
+          Register
+        </Button>
+      </ButtonGroup>
+    </FormBox>
+}`,...(n=(s=o.parameters)==null?void 0:s.docs)==null?void 0:n.source}}};const f=["StoryFormBox"];export{o as StoryFormBox,f as __namedExportsOrder,j as default};
