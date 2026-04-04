fbd5dabb85ddfdc03d89af27d78479806ccfc05e
BilLogic <BilLogic@users.noreply.github.com>
2026-04-04 19:30:12 +0000

    deploy: 86e32fd097ae013c0e695568f01cdbb7fa1fd79b

---
+++ storybook/assets/Input.stories-DsvXSLoo.js
@@ -0,0 +1,12 @@
+import{j as n}from"./jsx-runtime-BO8uF4Og.js";import{l as s}from"./Grid-CpMv2rNW.js";import"./index-D4H_InIO.js";import"./index-CZVi18Wq.js";import"./index-Dd8bRu6S.js";import"./useMediaQuery-BLMSMJgh.js";const u={component:s,title:"SDS Primitives/Inputs",parameters:{layout:"centered"}},e={name:"Input Field",args:{label:"I am a field",description:"I am a description",isDisabled:!1,placeholder:"I am a placeholder",isRequired:!1,type:"email"},render:i=>n.jsx(s,{...i})};var a,r,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  name: "Input Field",
+  args: {
+    label: "I am a field",
+    description: "I am a description",
+    isDisabled: false,
+    placeholder: "I am a placeholder",
+    isRequired: false,
+    type: "email"
+  },
+  render: args => <InputField {...args} />
+}`,...(t=(r=e.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const I=["StoryInputField"];export{e as StoryInputField,I as __namedExportsOrder,u as default};
